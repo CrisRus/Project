6ee22d49cbc921e87bacd8632a19e827f77ceea0
nahidalamgh <nahidalamgh@yahoo.com>
2021-06-02 12:47:38 +0200

    change fxml for guestinfo

--- src/Controllers/LoginController.java
+++ src/Controllers/LoginController.java
@@ -4,14 +4,10 @@ import DatabaseConnection.UserQueries;
 import Model.SceneSwitcher;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+
 import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
